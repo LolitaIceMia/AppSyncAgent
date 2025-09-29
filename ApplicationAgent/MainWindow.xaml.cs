cf1737110c9c25b8cc52ed47ba2d8fe20b9c5ea6
狐瓜西 <724095698ice@gmail.com>
2025-09-29 16:56:49 +0800

    Save！

---
+++ ApplicationAgent/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ToolSyncAgent; // 你的项目命名空间
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<SoftwarePackage> Packages { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        // 3. 初始化这个列表，并添加一些测试数据
+        Packages = new ObservableCollection<SoftwarePackage>
+        {
+            new SoftwarePackage { Name = "Git", CurrentVersion = "2.44.0", Status = "已安装" },
+            new SoftwarePackage { Name = "7-Zip", CurrentVersion = "23.01", Status = "已安装" },
+            new SoftwarePackage { Name = "FFmpeg", CurrentVersion = "N/A", Status = "未安装" },
+        };
+
+        this.DataContext = this;
+    }
+}
---
+++ ApplicationAgent/SoftwarePackage.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolSyncAgent;
+
+public class SoftwarePackage
+{
+    // { get; set; } 是一种简写，让这个属性可以被读取和写入
+    public string Name { get; set; }
+    public string CurrentVersion { get; set; }
+    public string Status { get; set; }
+}
